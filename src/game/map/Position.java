81e43857a5aec070b5d6d2290c1f921dc7152583
Oleg Magit <132854533+Oleg-Magit@users.noreply.github.com>
2025-04-15 17:45:33 +0300

    Update Position.java
    fbcfvb

--- src/game/map/Position.java
+++ src/game/map/Position.java
@@ -1,4 +1,5 @@
 package game.map;
+import java.util.Objects;
 
 public class Position {
     private int row;
@@ -7,15 +8,22 @@ public class Position {
         this.row=row;
         this.col=col;
     }
-    public int distanceTo(Position other) {
+    public int distanceTo(Position other){
         return Math.abs(this.row-other.row)+Math.abs(this.col-other.col);
-
     }
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(Object o){
         if(this==o){return true;}
         if(!(o instanceof Position)) return false;
         Position other = (Position) o;
         return this.row == other.row && this.col== other.col;
     }
+    @Override
+    public int hashCode(){
+        return Objects.hash(row,col);
+    }
+    @Override
+    public String toString(){
+        return "("+ row +","+col+")";
+    }
 }
