f4e8d9f92c0a4c49cf4a0fbc303acac2120e98ed
papesh82 <pimirila@gmail.com>
2025-04-15 15:57:21 +0300

    Update Position.java
    position class

--- src/game/map/Position.java
+++ src/game/map/Position.java
@@ -1,4 +1,31 @@
 package game.map;
 
 public class Position {
+    private int row;
+    private int col;
+    public Position(int row,int col){
+        this.row=row;
+        this.col=col;
+    }
+    public int distanceTo(Position other)
+    {
+        return Math.abs(this.row-other.row)+Math.abs(this.col-other.col);
+
+    }
+    @Override
+    public boolean equals(Object o)
+    {
+        if(this==o){return true;}
+        if(!(o instanceof Position)) return false;
+        Position other = (Position) o;
+        return this.row == other.row && this.col== other.col;
+
+
+
+    }
+
+    {
+
+    }
+
 }
